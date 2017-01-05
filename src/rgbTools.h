a802720aff2b48687ada568a80f4d25a48b58660
Aaron N <porkyneal@gmail.com>
2017-01-05 21:02:32 +0000

    remove excess headers

--- src/rgbTools.h
+++ src/rgbTools.h
@@ -2,8 +2,6 @@
   #define RGB_TOOLS // To make sure you don't declare the function more than once by including the header multiple times.
   #include "Arduino.h"
   #include "colours.h"
-  #include <math.h>
-  #include "colours.h"
 
   void setupRGB (int r_pin, int g_pin, int b_pin, double initialBrightness);
   void fadeKelvin(int to_kelvin, int timespan);
